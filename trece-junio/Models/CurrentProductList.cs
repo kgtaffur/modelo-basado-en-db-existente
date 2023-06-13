621b4e4d399589af7e2fc797a1dfb1a095cd1b39
Kevin <86487929+tar83@users.noreply.github.com>
2023-06-13 17:07:07 -0500

    Add project files.

---
+++ trece-junio/Models/CurrentProductList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace trece_junio.Models;
+
+public partial class CurrentProductList
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+}
